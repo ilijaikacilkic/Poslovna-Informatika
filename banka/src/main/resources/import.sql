5a6fcba4037125af470a0231942259d61f0c387d
ilijaikacilkic <37101107+ilijaikacilkic@users.noreply.github.com>
2022-01-07 21:53:18 +0100

    feat: changed hibernate.ddl-auto from update to create-drop and imported mySQL file

---
+++ banka/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into valuta (id, drzava, naziv, skraceni_naziv) values (1, "Srbija", "Dinar", "RSD");
+insert into valuta (id, drzava, naziv, skraceni_naziv) values (2, "Amerika", "Dolar", "USD");
+insert into valuta (id, drzava, naziv, skraceni_naziv) values (3, "Hrvatska", "Kuna", "KN");
+insert into delatnost (id, naziv, sifra) values (1, "aaa", "a123");
+insert into delatnost (id, naziv, sifra) values (2, "bbb", "b123");
+
+
