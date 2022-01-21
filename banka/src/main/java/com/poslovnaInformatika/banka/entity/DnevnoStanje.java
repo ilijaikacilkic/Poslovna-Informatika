ee431665cf4dc602fd20332e47f364444e05021d
Nenad <skavez.skavio98@gmail.com>
2022-01-21 18:07:05 +0100

    feat: added annotations to the class DnevnoStanje

--- banka/src/main/java/com/poslovnaInformatika/banka/entity/DnevnoStanje.java
+++ banka/src/main/java/com/poslovnaInformatika/banka/entity/DnevnoStanje.java
@@ -2,25 +2,44 @@ package com.poslovnaInformatika.banka.entity;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@Table(name = "dnevnostanje")
 public class DnevnoStanje {
 	
 	@Id
+	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private long id;
+	@Column(name = "datum")
 	private Date datum ;
+	
+	@Column(name = "predhodnoStanje")
 	private double predhodnoStanje ;
+	
+	@Column(name = "stanjeUkorist")
 	private double stanjeUkorist;
-	private double stanjeTereta ; 
+	
+	@Column(name = "stanjeTereta")
+	private double stanjeTereta ;
+	
+	@Column(name = "novoStanje")
 	private double novoStanje;
 	
 	
 	public DnevnoStanje() {
+		
 		
 	}
 	
