0c9940cb8899ab0d4ae0edb781cdd63c930be859
Nenad <skavez.skavio98@gmail.com>
2022-01-21 20:40:08 +0100

    feat:Add class GasenjeRacunaRepository

---
+++ banka/src/main/java/com/poslovnaInformatika/banka/repository/GasenjeRacunaRepository.java
@@ -0,0 +1,14 @@
+package com.poslovnaInformatika.banka.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.poslovnaInformatika.banka.entity.Gasenje_racuna;
+
+public interface GasenjeRacunaRepository extends JpaRepository <Gasenje_racuna, Long> {
+
+	
+	
+	
+	
+	
+}
