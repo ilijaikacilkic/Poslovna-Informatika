fc96c62c1e7caaebf916e7803c47698405d5570f
ilijaikacilkic <37101107+ilijaikacilkic@users.noreply.github.com>
2022-03-03 16:14:33 +0100

    refactor: changed header with to 100% and disabled border radius, imported FormsModule

--- angularEBank/src/app/app.module.ts
+++ angularEBank/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +22,7 @@ import { BankaComponent } from './components/banka/banka.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     MatSnackBarModule
   ],
   providers: [],
